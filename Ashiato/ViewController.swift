d85068ef63a4ff4926c55d189ad4631c6ba7ddd1
hebereke <antny0202@gmail.com>
2021-08-28 13:51:40 +0900

    Initial Commit

---
+++ Ashiato/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ashiato
+//
+//  Created by 山田純平 on 2021/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
