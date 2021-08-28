87e10606e747d42f8afb2635ec3a10396e5eadc2
Hajime <itorg110@gmail.com>
2021-08-28 21:34:18 +0900

    StoryboardからUIViewControllerを生成するextensionを追加

---
+++ Ashiato/extension/UIViewController+Extension.swift
@@ -0,0 +1,51 @@
+//
+//  UIViewController+Extension.swift
+//  Ashiato
+//
+//  Created by Hajime Ito on 2021/08/28.
+//
+
+import UIKit
+
+// MARK: StoryboardからUIViewControllerを生成
+public enum StoryboardInstantiateType {
+    case initial
+    case identifier(String)
+}
+
+public protocol StoryboardInstantiatable {
+    static var storyboardName: String { get }
+    static var storyboardBundle: Bundle { get }
+    static var instantiateType: StoryboardInstantiateType { get }
+}
+
+public extension StoryboardInstantiatable where Self: NSObject {
+    static var storyboardName: String {
+        className
+    }
+
+    static var storyboardBundle: Bundle {
+        Bundle(for: self)
+    }
+
+    private static var storyboard: UIStoryboard {
+        UIStoryboard(name: storyboardName, bundle: storyboardBundle)
+    }
+
+    static var instantiateType: StoryboardInstantiateType {
+        .identifier(className)
+    }
+}
+
+public extension StoryboardInstantiatable where Self: UIViewController {
+    static func instantiate() -> Self {
+        switch instantiateType {
+        case .initial:
+            return storyboard.instantiateInitialViewController() as! Self
+        case .identifier(let identifier):
+            return storyboard.instantiateViewController(withIdentifier: identifier) as! Self
+        }
+    }
+}
+
+extension UIViewController: StoryboardInstantiatable {}
