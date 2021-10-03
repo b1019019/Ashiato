ab800d8b229cd97825dd0ddf9c35bf125e26a416
hebereke <antny0202@gmail.com>
2021-10-03 14:19:35 +0900

    フォルダ整理

--- Ashiato/model/TaskData.swift
+++ Ashiato/model/TaskData.swift
@@ -51,19 +51,17 @@ let calendar = Calendar(identifier: .gregorian)
 
 var today = calendar.date(from: DateComponents(year: 2021, month: 8, day: 21))!
 
-var branch = TaskData(title: "延長", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 25, completeYear: 2021, completeMonth: 8, completeDay: 30, status: 0, nextTask: nil, branchTask:nil, extendHistory: nil)
+var branch = TaskData(title: "分岐子タスク", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 25, completeYear: 2021, completeMonth: 8, completeDay: 30, status: 0, nextTask: nil, branchTask:nil, extendHistory: nil)
 
 var branchTask = [branch]
 
-var nextTask = TaskData(title: "継続タスク子供", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 24, completeYear: 2021, completeMonth: 8, completeDay: 29, status: 2, nextTask: nil, branchTask: nil, extendHistory: nil)
+var nextTask = TaskData(title: "継続子タスク", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 24, completeYear: 2021, completeMonth: 8, completeDay: 29, status: 2, nextTask: nil, branchTask: nil, extendHistory: nil)
 
 var taskDataArray: [TaskData] = [
-        TaskData(title: "21日からの予定", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 21, completeYear: 2021, completeMonth: 8, completeDay: 23, status: 1, nextTask: nil, branchTask: nil, extendHistory: nil),
-        TaskData(title: "21日からの予定２", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 21, completeYear: 2021, completeMonth: 8, completeDay: 24, status: 2, nextTask: nil, branchTask: nil, extendHistory: nil),
         //延長つき
-        TaskData(title: "22日からの予定", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 22, completeYear: 2021, completeMonth: 8, completeDay: 25, status: 0, nextTask: nil, branchTask: [], extendHistory: [calendar.date(from: DateComponents(year: 2021, month: 8, day: 23))!, calendar.date(from: DateComponents(year: 2021, month: 8, day: 25))!]),
+        TaskData(title: "22日からの予定", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 22, completeYear: 2021, completeMonth: 8, completeDay: 25, status: 0, nextTask: nil, branchTask: nil, extendHistory: [calendar.date(from: DateComponents(year: 2021, month: 8, day: 23))!, calendar.date(from: DateComponents(year: 2021, month: 8, day: 25))!]),
         //分岐タスク付き
-        TaskData(title: "21日からの予定", memo: "分岐タスクメモ", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 21, completeYear: 2021, completeMonth: 8, completeDay: 23, status: 1, nextTask: nil, branchTask: branchTask, extendHistory: nil),
+        TaskData(title: "分岐元タスク", memo: "分岐タスクメモ", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 21, completeYear: 2021, completeMonth: 8, completeDay: 23, status: 1, nextTask: nil, branchTask: branchTask, extendHistory: nil),
         branch,
         TaskData(title: "継続タスク親", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 21, completeYear: 2021, completeMonth: 8, completeDay: 22, status: 2, nextTask: nextTask, branchTask: nil, extendHistory: nil),
         TaskData(title: "21日からの予定", memo: "えびです", achievementStandard: "達成基準", necessity: "必要性", startYear: 2021, startMonth: 8, startDay: 21, completeYear: 2021, completeMonth: 8, completeDay: 23, status: 1, nextTask: nil, branchTask: nil, extendHistory: nil),
